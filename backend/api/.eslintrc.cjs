d2c91171d28a56a3acdfd9694edd2e1a33c1aa57
Yuki <takamura88230820@gmail.com>
2026-01-13 23:22:37 +0900

    chore: eslintの設定ファイル追加

--- backend/api/.eslintrc.cjs
+++ backend/api/.eslintrc.cjs
@@ -1,10 +1,7 @@
 module.exports = {
   root: true,
   env: { node: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-  ],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
   ignorePatterns: ['dist', '.eslintrc.cjs', 'prisma'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
@@ -15,4 +12,4 @@ module.exports = {
     '@typescript-eslint/no-explicit-any': 'warn',
     '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
   },
-}
+};
